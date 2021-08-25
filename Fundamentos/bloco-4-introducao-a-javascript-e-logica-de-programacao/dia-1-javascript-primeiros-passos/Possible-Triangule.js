dd5c1a17b2a19f92646102847d75b87062a5ea1c
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-08-25 18:47:45 -0300

    Adiciona Possible-Triangule.js

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Possible-Triangule.js
@@ -0,0 +1,10 @@
+const firstValue = 50;
+const secondValue = 80;
+const thirdValue = 50;
+
+if (firstValue + secondValue + thirdValue === 180) {
+    console.log(true);
+}
+else{
+    console.log(false);
+}
