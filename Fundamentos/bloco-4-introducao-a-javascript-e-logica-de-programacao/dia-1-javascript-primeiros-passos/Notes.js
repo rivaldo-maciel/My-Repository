e5888d964823305c2562990489009a5678518703
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-08-25 19:04:28 -0300

    Adiciona Notes.js

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/Notes.js
@@ -0,0 +1,22 @@
+
+const allNotes = 30;
+
+
+if (allNotes >= 90){
+    console.log("A");
+}
+else if (allNotes >= 80){
+    console.log("B");
+}
+else if (allNotes >= 70) {
+    console.log("C");
+}
+else if (allNotes >= 60) {
+    console.log("D");
+}
+else if (allNotes >= 50) {
+    console.log("E");
+}
+else if (allNotes < 50) {
+    console.log("F");
+}
