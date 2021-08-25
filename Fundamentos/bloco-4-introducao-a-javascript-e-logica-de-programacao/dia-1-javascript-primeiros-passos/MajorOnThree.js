4047243e6b3de031368c6c472552c8b43db32b3e
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-08-25 18:34:37 -0300

    Adiciona MajorOnThree.js

---
+++ Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/MajorOnThree.js
@@ -0,0 +1,13 @@
+const num1 = 1;
+const num2 = 2;
+const num3 = 3;
+
+if (num1 > num2 && num1 > num3) {
+    console.log(num1);
+}
+else if (num2 > num1 && num2 > num3) {
+    console.log(num2);
+}
+else if (num3 > num1 && num3 >num2) {
+    console.log(num3);
+}
