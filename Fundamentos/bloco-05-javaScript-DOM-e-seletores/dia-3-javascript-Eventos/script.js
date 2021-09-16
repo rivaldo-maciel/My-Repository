64e30b3c11f79f2d57decff61feb9087524131bf
rivaldo-maciel <rivaldomaciel@hotmail.com>
2021-09-16 18:19:44 -0300

    commit

--- Fundamentos/bloco-05-javaScript-DOM-e-seletores/dia-3-javascript-Eventos/script.js
+++ Fundamentos/bloco-05-javaScript-DOM-e-seletores/dia-3-javascript-Eventos/script.js
@@ -21,10 +21,115 @@ function createDaysOfTheWeek() {
   for (let index = 0; index < dezDaysList.length; index+=1){
       let daysOfMonth = dezDaysList[index]
       let listDays = document.createElement('li')
+      listDays.className = "day"
       listDays.innerHTML = daysOfMonth
 
       dias.appendChild(listDays)
   }
+  document.querySelector('#days > li:nth-child(26)').classList.add('day','holiday')
+  document.querySelector('#days > li:nth-child(27)').classList.add('day', 'holiday', 'friday')
+  document.querySelector('#days > li:nth-child(33)').classList.add('day', 'holiday')
+
+  document.querySelector('#days > li:nth-child(6)').classList.add('day', 'friday')
+  document.querySelector('#days > li:nth-child(13)').classList.add('day', 'friday')
+  document.querySelector('#days > li:nth-child(20)').classList.add('day', 'friday')
+
+
   }
 
   creatDays()
+
+  function createHoliday(){
+    let divSelect = document.querySelector('.buttons-container')
+    let button = document.createElement('button')
+    button.innerHTML = "Feriados"
+    button.id = "btn-holiday"
+
+    divSelect.appendChild(button)
+  }
+
+  createHoliday()
+
+  function changeCollorHoliday(){
+   let daysHoliday = document.querySelectorAll(".holiday")
+   let button = document.querySelector('#btn-holiday')
+   let colorInitial = 'rgb(238,238,238)'
+   let colorChange = 'blue'
+
+   button.addEventListener("click", function(){
+   for (let index = 0; index < daysHoliday.length; index +=1){
+       if (daysHoliday[index].style.backgroundColor === colorChange){
+         daysHoliday[index].style.backgroundColor = colorInitial
+       }else{
+         daysHoliday[index].style.backgroundColor = colorChange
+       }
+   }
+  })
+}
+   
+changeCollorHoliday()
+
+
+function buttonFriday (){
+  let divSelect = document.querySelector('.buttons-container');
+  let button = document.createElement('button')
+  button.id = "btn-friday"
+  button.innerHTML = 'Sexta-Feira'
+  divSelect.appendChild(button)
+}
+
+buttonFriday()
+
+function changeColorFriday(){
+let daysSelect = document.querySelectorAll('.friday')
+let button = document.querySelector('#btn-friday')
+let colorInitial = 'rgb(238,238,238)'
+let colorChange = 'red'
+
+button.addEventListener("click", function(){
+for (let index = 0; index < daysSelect.length; index+=1){
+  if (daysSelect[index].style.backgroundColor === colorChange){
+    daysSelect[index].style.backgroundColor = colorInitial
+  }else{
+    daysSelect[index].style.backgroundColor = colorChange
+  }
+}
+})
+}
+
+changeColorFriday()
+
+
+function zoomDays(){
+  let daysSelect = document.querySelector('#days')
+
+  daysSelect.addEventListener('mouseover', function(event){
+    event.target.style.fontSize = '30px'
+    event.target.style.fontWeight = '600'
+  })
+}
+
+function zoomOutDays(){
+  let days = document.querySelector('#days')
+
+  days.addEventListener('mouseout', function(event){
+    event.target.style.fontSize = '20px'
+    event.target.style.fontWeight = '200'
+  })
+
+}
+
+zoomOutDays()
+zoomDays()
+
+
+function newTaskSpan(task) {
+
+  let tasksContainer = document.querySelector('.my-tasks');
+  let taskName = document.createElement('span');
+
+  taskName.innerHTML = task;
+  tasksContainer.appendChild(taskName);
+};
+
+newTaskSpan('Projeto:');
